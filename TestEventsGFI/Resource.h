af3c40433ec89f9088217d5d8df4006ff1a047aa
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2022-04-19 10:07:26 -0400

    Add files via upload

---
+++ TestEventsGFI/Resource.h
@@ -0,0 +1,8 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_SERIAL_NUMBER						1
+#define IDC_CANCEL                              40000
+#define IDC_EDIT                                40002
+#define IDC_OK                                  40003

8f05fde33d28bf5b7f23ccb3e94f38de865289b0
Phillip Smelt <98059794+Phillip-Smelt@users.noreply.github.com>
2022-04-28 13:51:45 -0400

    Delete TestEventsGFI directory

--- TestEventsGFI/Resource.h
+++
@@ -1,8 +0,0 @@
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
-
-#define IDD_SERIAL_NUMBER						1
-#define IDC_CANCEL                              40000
-#define IDC_EDIT                                40002
-#define IDC_OK                                  40003
